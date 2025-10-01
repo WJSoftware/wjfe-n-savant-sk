6fd7451af0b5f5def56f45506e8ebbabe2af68a3
Ramirez Vargas, José Pablo <webJose@gmail.com>
2025-10-01 14:27:33 -0600

    feat: Add the `SkFallback` Svelte component

---
+++ src/lib/SkFallback.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { browser } from '$app/environment';
+    import { Fallback } from '@wjfe/n-savant';
+    import type { ComponentProps } from 'svelte';
+
+    let { when, ...restProps }: ComponentProps<typeof Fallback> = $props();
+</script>
+
+<Fallback when={(rs, noMatches) => browser && (when?.(rs, noMatches) || (!when && noMatches))} {...restProps} />
